c951373b806669e9e294a0b7996757130ad18df3
Ayaan Adrito <ayaan.adrito@gmail.com>
2023-10-26 22:56:06 -0400

    Need to fix error on exercise 10a

--- list_exercises.py
+++ list_exercises.py
@@ -43,7 +43,7 @@ def mean(num_list):
     return sum(num_list) / len(num_list)
 
 def length_of_strings(string_list):
-    
+    '''Returns a list of lengths of the each item in the string list'''
     result = []
     
     for i in range(len(string_list)):
@@ -52,6 +52,8 @@ def length_of_strings(string_list):
     return result
 
 def mean_length_of_strings(string_list):
+    '''Calculates the average length of all the words in a string list'''
+
     string_lengths = length_of_strings(string_list)
     sum = 0
     
@@ -61,6 +63,8 @@ def mean_length_of_strings(string_list):
     return num / len(string_lengths)
 
 def count(lst, item):
+    '''Checks to see how many times a selected character appears in a string'''
+
     counter = 0
     
     for n in lst:
@@ -69,10 +73,35 @@ def count(lst, item):
             
     return counter
 
+def median(num_list):
+    '''Returns the median number in a list'''
+
+    num_list.sort()
+    n = len(num_list)
+    m = n // 2
+    if n % 2 == 0:
+        return (num_list[m - 1] + num_list[m]) / 2
+    else:
+        return num_list[m]
+    
+def median_length_of_strings(string_list):
+    '''Returns hte median length in a string list'''
+    return median(length_of_strings(string_list))
+
+def parallel_lists(stdns, ids):
+
+    print("CLASS LIST\n" + "-"*10)
+    print("{:{width}}" .format("Student", width=15), end="")
+    print("ID")
+    for i in range (len(stdns)):
+        print("{:{width}}".format(stdns[i], width=15), end="")
+        print(ids[i])
+
 def main():
-    print("Hello World!")
-    test = count(["a", "b", "b", "a", "c", "b"], "b")
-    print(test)
+    students = ["Arthur M", "Dutch V", "John M", "Sadie A", "Micah B"]
+    student_ids = {"26734", "12569", "46882", "09869", "57425"}
+
+    parallel_lists(students, student_ids)
 
 if __name__ == "__main__":
     main()
