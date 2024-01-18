aa5140db4b994a09ca2c70ddc42bf01c2237b30b
Ayaan Adrito <ayaan.adrito@gmail.com>
2024-01-18 13:53:48 -0500

    Debug exam question 6

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,35 @@
-names = {"Talia": 16, "Nabil": 15, "Rogan": 17} 
+score = 90
+grade = ''
 
-names.pop("Rogan")
+if score >= 93:
+    grade = 'A'
+if (score >= 84 or score <= 92):
+	grade = 'B'
+if (score >= 75 or score <= 83):
+	grade = 'C'
+if (score < 75):
+	grade = 'F'
+	
+print(grade)
 
-print(names)
+if (score >= 93):
+	grade = 'A'
+if (84 <= score <= 92):
+	grade = 'B'
+if (75 <= score <= 83):
+	grade = 'C'
+if (score < 75):
+	grade = 'F'
+	
+print(grade)
+
+if (score > 92):
+	grade = 'A'
+elif (score > 83):
+	grade = 'B'
+elif (score > 74):
+	grade = 'C'
+else:
+	grade = 'F'
+
+print(grade)
