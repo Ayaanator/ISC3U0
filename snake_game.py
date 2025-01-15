1d1f9b79215d4ef89264836255b0b4432511dd50
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-01-14 22:47:10 -0500

    Add Jake's connect four game

--- snake_game.py
+++ snake_game.py
@@ -1,35 +1,30 @@
-score = 90
-grade = ''
+def letter_counter(words: list):
+	dictionary = {}
 
-if score >= 93:
-    grade = 'A'
-if (score >= 84 or score <= 92):
-	grade = 'B'
-if (score >= 75 or score <= 83):
-	grade = 'C'
-if (score < 75):
-	grade = 'F'
-	
-print(grade)
+	for word in words:
+		for char in word:
+			dictionary[char] = dictionary.setdefault(char, 0) + 1
 
-if (score >= 93):
-	grade = 'A'
-if (84 <= score <= 92):
-	grade = 'B'
-if (75 <= score <= 83):
-	grade = 'C'
-if (score < 75):
-	grade = 'F'
-	
-print(grade)
+	return dictionary
 
-if (score > 92):
-	grade = 'A'
-elif (score > 83):
-	grade = 'B'
-elif (score > 74):
-	grade = 'C'
-else:
-	grade = 'F'
 
-print(grade)
+spam = {"title" : 23, "name": "arthur"}
+spam["title"] += 5
+spam["noob"] = ""
+
+names = {"Talia": 16, "Nabil": 15, "Rogan": 17} 
+names["Nolan"] = names.setdefault("Nolan", 0) + 152
+
+names["Nolan"] = names.setdefault("Nolan", 0) + 1253
+names[5] = "adads"
+
+print(names)
+
+def mystery(nums: [int]):
+	for i in range(len(nums)):
+		nums[i] += 2
+
+x = [1,2,3,4]
+mystery(x)
+print(x)
+print(names[5])
